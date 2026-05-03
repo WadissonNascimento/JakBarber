24d61ab4f0eb96e6b723e5335540c822f6483761
Wadisson Nascimento <wadisson97.w.g@gmail.com>
2026-05-03 02:17:19 -0300

    torna logout link nativo

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,17 +1,10 @@
-"use client";
-
 export function LogoutButton() {
-  function handleLogout() {
-    window.location.assign("/logout");
-  }
-
   return (
-    <button
-      type="button"
-      onClick={handleLogout}
+    <a
+      href="/logout"
       className="inline-flex rounded-xl border border-red-700 px-4 py-2 text-sm text-red-400 transition hover:bg-red-700/10"
     >
       Sair
-    </button>
+    </a>
   );
 }
