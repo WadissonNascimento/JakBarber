79ce7930a97a6872294de8e62b86915337b7d191
Wadisson Nascimento <wadisson97.w.g@gmail.com>
2026-05-03 02:08:33 -0300

    corrige botao de logout

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link";
+"use client";
 
 export function LogoutButton() {
+  function handleLogout() {
+    window.location.assign("/logout");
+  }
+
   return (
-    <Link
-      href="/logout"
+    <button
+      type="button"
+      onClick={handleLogout}
       className="inline-flex rounded-xl border border-red-700 px-4 py-2 text-sm text-red-400 transition hover:bg-red-700/10"
     >
       Sair
-    </Link>
+    </button>
   );
 }
