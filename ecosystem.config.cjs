beb12c0e3c8a0fb2b05fb6a3305f14be534b0bed
Wadisson Nascimento <wadisson97.w.g@gmail.com>
2026-05-10 17:31:27 -0300

    Restringe Next ao localhost no PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "jakbarber",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -H 127.0.0.1 -p 3000",
       exec_mode: "fork",
       instances: 1,
       max_memory_restart: "450M",
